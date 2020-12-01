47a6dfb654dae15603e8b5de9500cda419b15254
Eduarjavis <geej5791@gmail.com>
2020-11-30 23:29:29 -0600

    PracticaPibes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,25 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
+  templateUrl: './app.component.html'})
 export class AppComponent {
-  title = 'PRACTICAPIBES';
+  title = 'practicasBasicasPipes';
+  name='sheldon';
+  myArray=[1,2,3,4,5,6];
+  PI=Math.PI;
+  myNum=0.589;
+  salary=3500.5;
+  myDate=new Date();
+  video:string='o-0ygW-B_gI';
+
+  nerd = {​​​​ 
+    name: 'Sheldon', 
+    alias: 'Shelly', 
+    song: 'Soft Kitty', 
+    skills: ['eidetic memory', 'makes people nervous'], 
+    youtubeChannel: 'Fun With Flags', 
+    address: {​​​​ street: 'Elm Street', 
+    number: 3, 
+    city: 'Pasadena' }​​​​ 
+  }​​​​;
 }
